cee77769db8fdd093e5e0311e3a18d21767c6ac9
mbarablin <mbarablin@test.me>
2020-05-23 16:32:44 +0300

    Now we have 3 commands: list, last, info

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,45 @@ use std::str::FromStr;
 use mime::Mime;
 extern crate serde_json;
 
-fn parse_json_to_dir_list(r:reqwest::blocking::Response) -> Vec<String> {
+const BASE_API_URL: &'static str = "https://cloud-api.yandex.net:443/v1/disk";
+
+fn make_api_request(url: &str, oauth_token: &str) -> Result<String, Box<dyn std::error::Error>> {
+    println!("Making API request: {}", url);
+    let rclient = reqwest::blocking::Client::new();
+    let resp = rclient.get(url)
+        .header(reqwest::header::AUTHORIZATION, format!("OAuth {}", oauth_token))
+        .send()?;
+
+    if resp.status() == reqwest::StatusCode::OK {
+        println!("OK");
+        let ct = Mime::from_str(resp.headers().get(reqwest::header::CONTENT_TYPE).unwrap().to_str()?)?;
+        if (ct.type_() == mime::APPLICATION) && (ct.subtype() == mime::JSON) {
+            Ok(resp.text()?)
+        } else {
+            println!("--Mime type is not application/json");
+            Err("Mime type is not application/json".to_string().into())
+        }
+    } else {
+        println!("--Responce status is not OK");
+        Err("Responce status is not OK".to_string().into())
+    }
+}
+
+fn get_info(url: &str, oauth_token: &str) -> Result<(), Box<dyn std::error::Error>>{
+    println!("Yandex disk info:\n{:#?}", serde_json::from_str::<serde_json::Value>(
+        make_api_request(url, oauth_token)?.as_str()));
+    Ok(())
+}
+
+fn get_last(url: &str, _oauth_token: &str) -> Result<(), Box<dyn std::error::Error>>{
+    println!("Url for get_last: {}", url);
+    println!("Under construction!");
+    Ok(())
+}
+
+fn parse_json_text_to_dir_list(s: &str) -> Vec<String> {
     // FIXME refactor without unwraps
-
-    let j: serde_json::Value = r.json().unwrap();
-
+    let j: serde_json::Value = serde_json::from_str::<serde_json::Value>(s).unwrap();
     let items = j["_embedded"]["items"].as_array().unwrap();
     
     items.into_iter()
@@ -20,34 +54,10 @@ fn parse_json_to_dir_list(r:reqwest::blocking::Response) -> Vec<String> {
         .collect()
 }
 
-fn get_last(_url: &str, _oauth_token: &str) -> Result<(), Box<dyn std::error::Error>>{
-    println!("Under construction!");
-    Ok(())
-}
-
 fn get_list(url: &str, oauth_token: &str) -> Result<(), Box<dyn std::error::Error>>{
-    // making the request
-
-    let rclient = reqwest::blocking::Client::new();
-    let resp = rclient.get(url)
-        .header(reqwest::header::AUTHORIZATION, format!("OAuth {}", oauth_token))
-        .send()?;
-
-    match (resp.status(), resp.headers().get(reqwest::header::CONTENT_TYPE)) {
-        (reqwest::StatusCode::OK, Some(content_type)) => {
-            let content_type = Mime::from_str(content_type.to_str()?)?;
-            match (content_type.type_(), content_type.subtype()) {
-                (mime::APPLICATION, mime::JSON) => println!("Root dir contents:\n{}", parse_json_to_dir_list(resp).join(",\n")),
-                _ => println!("The reponse contains {:#?}.", (content_type.type_(), content_type.subtype())),
-            };
-
-            Ok(())
-        }
-        (_, _) => {
-            println!("The response status isn't OK or has no CONTENT-TYPE header.\nResponce STATUS:{}. Headers({:#?})", resp.status(), resp.headers());
-            Err("Error while fetching directory list via API".into())
-        }
-    }
+    println!("Root dir contents:\n{}", 
+        parse_json_text_to_dir_list(make_api_request(url, oauth_token)?.as_str()).join(",\n"));
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -69,6 +79,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 .value_name("URL")
                                 .help("Sets a custom Yandex Disk url")
                                 .takes_value(true))
+                            .subcommand(SubCommand::with_name("info"))
                             .subcommand(SubCommand::with_name("list")
                                 .about("Get directory listing")
                                 .arg(Arg::with_name("long")
@@ -82,15 +93,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             .subcommand(SubCommand::with_name("last")
                                 .about("Get last uploaded files"))
                             .get_matches();
+ 
+    let url = matches.value_of("url").unwrap_or(BASE_API_URL);
 
-    let url = matches.value_of("url").unwrap_or("https://cloud-api.yandex.net:443/v1/disk/resources?path=%2F");
-    println!("Value for url: {}", url);
     // TODO read token from config file
     let oauth_token = matches.value_of("oauth-token").unwrap();
         
     match matches.subcommand() {
-        ("list", _) => { get_list(url, oauth_token) },
-        ("last", _) => { get_last(url, oauth_token) },
+        ("list", _) => { get_list([url,"/resources?path=%2F"].concat().as_str(), oauth_token) },
+        ("last", _) => { get_last([url,"/resources/last-uploaded?limit=5"].concat().as_str(), oauth_token) },
+        ("info", _) => { get_info(url, oauth_token) },
         _ => {println!("No command given. Use help please."); Ok (())}
     }
 
