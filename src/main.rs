17832eabb0e88fcaf98e50624aef7fae92be5a94
mbarablin <mbarablin@test.me>
2020-10-13 20:38:18 +0300

    initial working threaded version of local file watcher

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ extern crate serde_json;
 #[macro_use]
 extern crate text_io;
 
-use notify::event::EventKind::*;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use std::fs;
@@ -19,6 +18,13 @@ use yandex_disk_api::*;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+use rusqlite::{params, Connection, Result, types::Null};
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+use md5::{Md5, Digest};
+use std::io::BufReader;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     yandex_disk_api_url: String,
@@ -28,7 +34,7 @@ pub struct Config {
     mounts: HashMap<String, MountPointConfig>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 struct MountPointConfig {
     local_path: String,
     remote_path: String,
@@ -119,8 +125,85 @@ fn _start_sync(){
      */
 }
 
-fn start_watch(/*path: &str, */mounts: &HashMap<String, MountPointConfig>) -> Result<(), Box<dyn std::error::Error>> {
-    //println!("Starting watch for: {}", path);
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct MountMeta {
+    local_revision: u128,
+    remote_revision: u128,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct FileMeta {
+    path: String, 
+    filename: String, 
+    discovered_at: i64,
+    synced_at: i64,
+    directory: bool,
+    md5sum: String,
+}
+
+fn init_mount_db(mount_name: &String) -> Result<Connection, Box<dyn std::error::Error>> {
+    //FIXME check mount_name format
+    let conn = Connection::open(format!("mount_{}.sqlite3", mount_name))?;
+
+    let r = conn.execute(
+        "CREATE TABLE IF NOT EXISTS mount_meta (
+            id              INTEGER PRIMARY KEY,
+            local_revision  INTEGER,
+            remote_revision INTEGER
+        );",
+        params![],
+    );
+
+    println!("create table mount_meta: {:#?}", r);
+        
+    let r = conn.execute(
+        "CREATE TABLE IF NOT EXISTS file_meta (
+            path            VARCHAR NOT NULL PRIMARY KEY,
+            filename        VARCHAR NOT NULL,
+            discovered_at   DATETIME,
+            synced_at       DATETIME,
+            directory       BOOLEAN,
+            md5sum          VARCHAR
+        );",
+        params![],
+    )?;
+
+    println!("create table mount_meta: {:#?}", r);
+        
+    match conn.execute(
+        "CREATE UNIQUE INDEX idx_file_meta_unique_path 
+         ON file_meta (path);",
+        params![],
+    ) {
+        Ok(_) => println!("{}: Index on filemeta created", mount_name),
+        Err(err) => println!("{}: Warning: failed to create index on file_meta. Error: {:#?}", mount_name, err),
+    }
+
+    println!("create unique index on file_meta: {:#?}", r);
+
+    conn.execute(
+        "INSERT OR IGNORE INTO mount_meta (id, local_revision, remote_revision) VALUES (?1, ?2, ?3)",
+        params![1, Null, Null],
+    )?;
+
+    Ok(conn)
+}
+
+fn discover_local_file(conn: &Connection, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let r = conn.execute(
+    "REPLACE INTO file_meta (path, filename, discovered_at, synced_at, directory, md5sum)
+     VALUES (?1, ?2, ?3, ?4, ?5, ?6);
+    ",
+    params![path, "filename", SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string(), Null, Null, Null]);
+        
+    println!("write file_meta record result: {:#?}", r);
+
+    Ok(())
+}
+
+// This function will start deferred directory watch
+fn start_mount_watch(mount_name: &String, mount: &MountPointConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let conn = init_mount_db(&mount_name)?;
 
     let (tx, rx) = std::sync::mpsc::channel();
 
@@ -130,31 +213,105 @@ fn start_watch(/*path: &str, */mounts: &HashMap<String, MountPointConfig>) -> Re
 
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
-    //watcher.watch(path, RecursiveMode::Recursive)?;
-
-    for (k,v) in mounts {
-        println!("Registring watch '{}': at '{}'", k, v.local_path);
-        watcher.watch(&v.local_path, RecursiveMode::Recursive)?;
-    }
+    watcher.watch(&mount.local_path, RecursiveMode::Recursive)?;
 
     for res in rx {
         match res {
-            Ok(event) => match event.kind {
-                Create(ck) => {
-                    println!("Create event. {:#?} : {:#?}", ck, event.paths);
-                }
-                Modify(_) => {
-                    println!("Modify event! {:#?}", event.paths);
-                }
-                Remove(_) => {
-                    println!("Remove event! {:#?}", event.paths);
-                }
-                _ => {
-                    println!("Other event! {:#?}", event);
-                }
+            Ok(event) =>  {
+                println!("{}:Other event! {:#?}", &mount_name, event);
+                discover_local_file(
+                    &conn,
+                    event.paths.first().unwrap().to_str().unwrap()
+                )?;
             },
             Err(event) => println!("watch error: {:?}", event),
         }
+    };
+    
+    Ok(())
+}
+
+fn process_files(mount_name: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let ten_sec = Duration::from_secs(10);
+
+    loop {
+        thread::sleep(ten_sec);
+
+        let conn = Connection::open(format!("mount_{}.sqlite3", mount_name))?;
+
+        let r = conn.execute(
+            "SELECT * FROM file_meta WHERE md5sum IS NULL;",
+            params![],
+        );
+        println!("{}: Select files to process: {:#?}", mount_name, r);
+
+        let mut stmt = conn.prepare("SELECT path, filename, discovered_at, synced_at, directory, md5sum FROM file_meta WHERE md5sum IS NULL;")?;
+        let filemeta_iter = stmt.query_map(params![], |row| {
+            Ok(FileMeta {
+                path: row.get(0)?,
+                filename: row.get(1)?,
+                discovered_at: row.get(2).unwrap_or(0),
+                synced_at: row.get(3).unwrap_or(0),
+                directory: row.get(4).unwrap_or(false),
+                md5sum: row.get(5).unwrap_or("".to_string()),
+            })
+        })?;
+
+        for filemeta in filemeta_iter {
+            let fm = filemeta.unwrap();
+            println!("{}: Found file to process {:#?}", mount_name, fm);
+
+            let mut hasher = Md5::new();
+            hasher.update(fs::read(&fm.path).unwrap());
+            let md5 = hasher.finalize();
+            println!("Digest(1 reads): {:x}", &md5);
+
+            conn.execute(
+                "UPDATE file_meta SET md5sum = ?1 
+                 WHERE path = ?2",
+                params![format!("{:x}", &md5), fm.path],
+            )?;
+        }
+    }
+}
+
+fn start_all_watches(mounts: &HashMap<String, MountPointConfig>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut watch_handlers = vec![];
+        
+    //watcher threads
+    for (k,v) in mounts {
+        let thread_mount_name = k.clone();
+        let thread_mount = v.clone();
+        watch_handlers.push(thread::spawn(move || {
+            println!("Registring watch '{}': at '{}'", thread_mount_name, thread_mount.local_path);
+            match start_mount_watch(&thread_mount_name, &thread_mount) {
+                Ok(_) => {
+                    println!("Watch for {} successfully done", thread_mount_name); "Ok"
+                },
+                Err(err) => {
+                    println!("Watch for {} was faulty. {:#?}", thread_mount_name, err); "Err"
+                },
+            }
+        }));
+
+        // file processor
+        let thread_mount_name = k.clone();
+        watch_handlers.push(thread::spawn(move || {
+            match process_files(&thread_mount_name) {
+                Ok(_) => {
+                    println!("File Processor for {} successfully done", thread_mount_name); "Ok"
+                },
+                Err(_) => {
+                    println!("File Processor for {} was faulty ", thread_mount_name); "Err"
+                },
+            }
+        }));
+
+    }
+    
+    for child in watch_handlers {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child.join();
     }
 
     Ok(())
@@ -297,7 +454,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         ("watch", _) => {
             println!("There will be watch!");
-            start_watch(&conf.mounts)?;
+            start_all_watches(&conf.mounts)?;
             Ok(())
         }
         _ => {
